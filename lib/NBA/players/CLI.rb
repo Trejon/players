f09b26556444dbb7c56883f4fa50a62854e614e0
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-07-30 17:17:34 +0000

    first commit

---
+++ lib/NBA/players/CLI.rb
@@ -0,0 +1,10 @@
+require "NBA/players/CLI/version"
+
+module NBA
+  module Players
+    module CLI
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/NBA/players/CLI/version.rb
@@ -0,0 +1,7 @@
+module NBA
+  module Players
+    module CLI
+      VERSION = "0.1.0"
+    end
+  end
+end
